9a575f65395604a117a1ee949f7807d12d492b8d
Tobi Dalhof <dalhof.tobias@gmail.com>
2021-01-23 14:27:24 +0000

    postgres naming conventions

--- api/postgres/migrations/V1__init_tables.sql
+++ api/postgres/migrations/V1__init_tables.sql
@@ -11,28 +11,28 @@ CREATE TABLE IF NOT EXISTS sv_room (
     id UUID PRIMARY KEY,
     owner_user_id UUID,
     name VARCHAR,
-    CONSTRAINT FK_SV_ROOM_OWNER_USER_ID FOREIGN KEY(owner_user_id) REFERENCES sv_user(id)
+    CONSTRAINT fkey_sv_room_owner_user_id FOREIGN KEY(owner_user_id) REFERENCES sv_user(id)
 );
 
 CREATE TABLE IF NOT EXISTS sv_room_user (
     room_id UUID NOT NULL,
     user_id UUID NOT NULL,
-    CONSTRAINT FK_SV_ROOM_USER_ROOM_ID FOREIGN KEY(room_id) REFERENCES sv_room(id) ON DELETE CASCADE,
-    CONSTRAINT FK_SV_ROOM_USER_USER_ID FOREIGN KEY(user_id) REFERENCES sv_user(id) ON DELETE CASCADE
+    CONSTRAINT fkey_sv_room_user_room_id FOREIGN KEY(room_id) REFERENCES sv_room(id) ON DELETE CASCADE,
+    CONSTRAINT fkey_sv_room_user_user_id FOREIGN KEY(user_id) REFERENCES sv_user(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS sv_queue_item (
     id UUID PRIMARY KEY,
     room_id UUID NOT NULL,
     user_id UUID NOT NULL,
-    CONSTRAINT FK_SV_QUEUE_ITEM_ROOM_ID FOREIGN KEY(room_id) REFERENCES sv_room(id) ON DELETE CASCADE,
-    CONSTRAINT FK_SV_QUEUE_ITEM_USER_ID FOREIGN KEY(user_id) REFERENCES sv_user(id) ON DELETE CASCADE
+    CONSTRAINT fkey_sv_queue_item_room_id FOREIGN KEY(room_id) REFERENCES sv_room(id) ON DELETE CASCADE,
+    CONSTRAINT fkey_sv_queue_item_user_id FOREIGN KEY(user_id) REFERENCES sv_user(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS sv_queue_item_vote (
     id UUID PRIMARY KEY,
     queue_item_id UUID NOT NULL,
     user_id UUID NOT NULL,
-    CONSTRAINT FK_SV_QUEUE_ITEM_VOTE_QUEUE_ITEM_ID FOREIGN KEY(queue_item_id) REFERENCES sv_queue_item(id) ON DELETE CASCADE,
-    CONSTRAINT FK_SV_QUEUE_ITEM_VOTE_USER_ID FOREIGN KEY(user_id) REFERENCES sv_user(id) ON DELETE CASCADE
+    CONSTRAINT fkey_sv_queue_item_vote_queue_item_id FOREIGN KEY(queue_item_id) REFERENCES sv_queue_item(id) ON DELETE CASCADE,
+    CONSTRAINT fkey_sv_queue_item_vote_user_id FOREIGN KEY(user_id) REFERENCES sv_user(id) ON DELETE CASCADE
 );
