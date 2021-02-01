2261c366ea4777ca7ce43c24ba754e3f1354c768
Tobi Dalhof <dalhof.tobias@gmail.com>
2021-02-01 06:30:08 +0000

    remove origin check

--- api/http/middleware/cors.go
+++ api/http/middleware/cors.go
@@ -2,18 +2,11 @@ package middleware
 
 import (
 	"net/http"
-
-	"github.com/syncedvideo/syncedvideo/http/response"
 )
 
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		origin := r.Header.Get("Origin")
-		if origin == "" {
-			response.WithError(w, "missing Origin header", http.StatusBadRequest)
-			return
-		}
-		w.Header().Set("Access-Control-Allow-Origin", origin)
+		w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
 		w.Header().Add("Access-Control-Allow-Methods", "*")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		next.ServeHTTP(w, r)
