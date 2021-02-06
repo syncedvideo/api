e7ce5031be2faac6b24a0fbb16a37fcf29790847
Tobi Dalhof <dalhof.tobias@gmail.com>
2021-02-06 07:26:22 +0000

    add more words

--- api/sillyname/sillyname.go
+++ api/sillyname/sillyname.go
@@ -2,8 +2,8 @@ package sillyname
 
 import "math/rand"
 
-var adjectives []string = []string{"Big", "Large", "Huge", "Small", "Gay", "Lesbian", "Tiny", "Black", "White", "Pink", "Crimson", "Red", "Maroon", "Brown", "Misty", "Rose", "Salmon", "Coral", "Chocolate", "Orange", "Gold", "Ivory", "Yellow", "Olive", "Chartreuse", "Lime", "Green", "Aquamarine", "Turquoise", "Azure", "Cyan", "Teal", "Lavender", "Blue", "Navy", "Violet", "Indigo", "Dark", "Plum", "Magenta", "Purple"}
-var nouns []string = []string{"Jerome", "Philipp", "Tobi", "Penis", "Dick", "Monkey", "Ape", "Retard"}
+var adjectives []string = []string{"Big", "Large", "Huge", "Small", "Swole", "Gay", "Lesbian", "Tiny", "Black", "White", "Pink", "Crimson", "Red", "Maroon", "Brown", "Misty", "Rose", "Salmon", "Coral", "Chocolate", "Orange", "Gold", "Ivory", "Yellow", "Olive", "Chartreuse", "Lime", "Green", "Aquamarine", "Turquoise", "Azure", "Cyan", "Teal", "Lavender", "Blue", "Navy", "Violet", "Indigo", "Dark", "Plum", "Magenta", "Purple"}
+var nouns []string = []string{"Jerome", "Philipp", "Tobi", "Penis", "Dick", "Boobs", "Ass", "Monkey", "Ape", "Retard", "German", "Gopher", "Marburger", "Dortmunder", "Schwabe"}
 
 func New() string {
 	randAdjective := adjectives[rand.Intn(len(adjectives))]
