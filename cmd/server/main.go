ba44d1be5e6f39a660d36a1d5c46829d0bae0449
Tobi Dalhof <dalhof.tobias@gmail.com>
2021-05-14 10:15:37 +0000

    init main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/syncedvideo/syncedvideo"
+)
+
+func main() {
+	store := syncedvideo.StubRoomStore{
+		Rooms: map[string]syncedvideo.Room{
+			"jerome": {ID: "jerome", Name: "Jeromes room"},
+		},
+	}
+	server := syncedvideo.NewServer(&store)
+	log.Fatal(http.ListenAndServe(":3000", server))
+}
